4892c518946c3441a1c1e9c7a6888f93c8b8089f
Tomas Peiretti <tomaspeiretti@gmail.com>
2021-08-13 10:40:38 -0300

    Se suben archivos

---
+++ App.py
@@ -0,0 +1,70 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from QtDragAndDropWidget import DragAndDropWidget
+
+
+class UI_MainWindow(QtWidgets.QMainWindow):
+
+    def setupUI(self, MainWindow):
+
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.wdg_file_drop = DragAndDropWidget(self.centralwidget)
+        self.wdg_file_drop.setGeometry(QtCore.QRect(10, 10, 611, 341))
+        self.wdg_file_drop.setObjectName("wdg_file_drop")
+        self.wdg_file_drop.tedit_path.textChanged.connect( lambda: self.btn_comenzar.setEnabled(True))
+
+        font = QtGui.QFont()
+        font.setFamily("Garuda")
+        font.setItalic(False)
+        font.setKerning(True)
+
+        self.btn_comenzar = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_comenzar.setGeometry(QtCore.QRect(490, 390, 131, 31))
+        self.btn_comenzar.setObjectName("btn_comenzar")
+        self.btn_comenzar.setEnabled(False)
+
+        self.btn_ayuda = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_ayuda.setGeometry(QtCore.QRect(20, 390, 100, 31))
+        self.btn_ayuda.setObjectName("btn_ayuda")
+        self.btn_ayuda.clicked.connect(self.showPopUp)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 25))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "TITULO"))
+        self.btn_comenzar.setText(_translate("MainWindow", "Comenzar"))
+        self.btn_ayuda.setText(_translate("MainWindow", "Ayuda"))
+
+    def showPopUp(self):
+        msg = QtWidgets.QMessageBox()
+        msg.setWindowTitle("Ayuda")
+        msg.setIcon(QtWidgets.QMessageBox.Question)
+        msg.setText("1) Descargue el archivo .csv del curso correspondiente en la página www.urionlinejudge.com.br/academic/, el cual se encuentra en el apartado 'Students' del curso seleccionado\n\n"+
+                    "2) Arraste el archivo en el programa y verifique que le aparezca la ubicacion del archivo en el diálogo correspondiente.\n\n"+
+                    "3) Presione el botón comenzar para procesar todos los usuarios y espere a que el proceso finalice.\n\n"+
+                    "4) Una vez finalizado el proceso, se creará un archivo .xls (excel) con los usuarios que no contengan el perfil con el país y/o la universidad completos.")
+        x = msg.exec_()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = UI_MainWindow(MainWindow)
+    ui.setupUI(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ QtDragAndDropWidget.py
@@ -0,0 +1,62 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QWidget
+import sys
+import os
+
+class DragAndDropWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setUpUI()
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+        if e.mimeData().hasUrls() and e.mimeData().urls().__len__() == 1:
+            e.accept()
+        else:
+            e.ignore()
+
+    def dropEvent(self, e):
+        url = e.mimeData().urls()[0].toLocalFile()
+        if url.find(".csv") != -1:
+            self.tedit_path.setText(url)
+        else:
+            self.showErrorPopUp()
+
+    def setUpUI(self):
+        self.setObjectName("DragAndDropWidget")
+        self.layout = QtWidgets.QGridLayout()
+        self.setLayout(self.layout)
+        
+        self.btn_buscar = QtWidgets.QPushButton(self)
+        self.btn_buscar.setGeometry(QtCore.QRect(255, 150, 131, 31))
+        self.btn_buscar.setObjectName("btn_buscar")
+        self.btn_buscar.clicked.connect(self.browsefiles)
+
+        self.lbl_arrastrar = QtWidgets.QLabel(self)
+        self.lbl_arrastrar.setGeometry(QtCore.QRect(100, 50, 440, 91))
+        self.lbl_arrastrar.setObjectName("lbl_arrastrar")
+
+        self.tedit_path = QtWidgets.QLineEdit(self)
+        self.tedit_path.setEnabled(False)
+        self.tedit_path.setGeometry(QtCore.QRect(20, 200, 591, 31))
+        self.tedit_path.setObjectName("tedit_path")
+
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("DragAndDropWidget", "DragAndDropWidget"))
+        self.btn_buscar.setText(_translate("DragAndDropWidget", "Buscar"))
+        self.lbl_arrastrar.setText(_translate("DragAndDropWidget", "<html><head/><body><p align=\"center\"><span style=\" font-size:16pt;\">Arrastre el archivo .csv aquí<br/>o</span></p></body></html>"))
+
+    def browsefiles(self):
+        file = QFileDialog.getOpenFileName(self,'Buscar archivo',os.getcwd(),'CSV Files (*.csv)')
+        self.tedit_path.setText(file[0])
+
+    def showErrorPopUp(self):
+        msg = QtWidgets.QMessageBox()
+        msg.setWindowTitle("Advertencia")
+        msg.setIcon(QtWidgets.QMessageBox.Warning)
+        msg.setText("Solo se pueden procesar archivos del tipo .csv")
+        x = msg.exec_()
